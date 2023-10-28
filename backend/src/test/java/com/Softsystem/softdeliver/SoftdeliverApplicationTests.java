60014cdf9750778e677e2046e9ece71ab60ba3ac
DanielChombo <danielchombo.dotordaniel@gmail.com>
2023-10-28 22:57:40 +0100

    create project

---
+++ backend/src/test/java/com/Softsystem/softdeliver/SoftdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Softsystem.softdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
